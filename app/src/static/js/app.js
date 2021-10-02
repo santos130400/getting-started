15d355b80525da45ade32b18da2552eceaed2b1b
santos130400 <santitobar2010@hotmail.com>
2021-10-02 07:07:51 -0500

    que sirva

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -2,6 +2,7 @@ function App() {
     const { Container, Row, Col } = ReactBootstrap;
     return (
         <Container>
+            <Text> Integrantes </Text>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
