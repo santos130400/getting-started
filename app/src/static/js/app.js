c349c6c4f6ca13a18f1ad7792ba1eac652a2cb9b
santos130400 <santitobar2010@hotmail.com>
2021-10-02 07:04:08 -0500

    .

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,8 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                  <><p className="text-center">You have no todo items yet! Add one above!</p>
-                  <h2 className="text-center"> Integrantes</h2></>
+                  <p className="text-center">You have no todo items yet! Add one above!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
